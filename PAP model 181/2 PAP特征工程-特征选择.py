9ff54297c91f6f530ff0ea9e9e588aa0b1e066be
will <59821233+DrugAP@users.noreply.github.com>
2024-06-25 01:22:50 +0800

    Add files via upload

---
+++ PAP model 181/2 PAP特征工程-特征选择.py
@@ -0,0 +1,94 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+from sklearn.ensemble import RandomForestClassifier     # 随机森林(Random Forest) BaggingRegressor是随机森林专用
+from sklearn.feature_selection import RFE
+
+# 特征选择：RFE
+# 1.导入数据
+df = pd.read_csv("F:/fluralaner_train_208descriptor.csv",index_col = 0)
+X = df.drop(columns=['Activity'])
+y = df['Activity']
+
+# 2.保存原始数据的列名
+column_names = df.columns[:-1]
+
+# 3.选择特征
+scores_and_params = []
+'''
+# 确定特征数量
+for i in range(60,121,10):
+    for j in range(4, 21, 4):
+        for k in range(5, 15, 1):
+            for u in range(1, 5, 1):
+                RFC_ = RandomForestClassifier(n_estimators=i,max_depth=j,min_samples_leaf=u,random_state=0)  # 随机森林实例化
+                selector = RFE(RFC_, n_features_to_select=k,step=10).fit(X,y)  # RFE实例化
+
+                selected_features_indices = selector.support_  # 返回所有的特征是否被选中的布尔矩阵
+                selected_feature_names = X.columns[selected_features_indices]  # 选中特征的列名
+
+                print("Selected feature names: ", selected_feature_names)
+                print("n_estimators: {0} \n max_depth: {1} \n n_features_to_select: {2} \n min_samples_leaf: {3}".format(i, j, k, u))
+
+                #print(selector.support_.sum())    # support_:返回所有的特征是否被选中的布尔矩阵,求和后得到30个特征
+                #print(selector.ranking_)        # ranking_:返回特征的按数次迭代中综合重要性的排名 排第1的特征最重要
+
+                # 4.转换数据
+                X_wrapper_features = selector.transform(X)
+
+                # 5.交叉验证
+                auc_scores = cross_val_score(RFC_,X_wrapper_features,y,cv=5,scoring='roc_auc').mean()
+
+                print(auc_scores)
+                scores_and_params.append((auc_scores, selected_feature_names, i, j, k, u))
+
+# 对得分进行排序并选择前十个得分
+scores_and_params_sorted = sorted(scores_and_params, key=lambda x: x[0], reverse=True)
+top_10_scores_and_params = scores_and_params_sorted[:10]
+
+# 打印前十个得分及其相应的参数组合
+for score, feature_names, n_estimators, max_depth, n_features_to_select, min_samples_leaf in top_10_scores_and_params:
+    print(f"Score: {score}, Features: {list(feature_names)}, n_estimators: {n_estimators}, "
+          f"max_depth: {max_depth}, n_features_to_select: {n_features_to_select}, min_samples_leaf: {min_samples_leaf}")
+
+'''
+RFC_ = RandomForestClassifier(n_estimators=90,max_depth=4 ,min_samples_leaf=4,random_state=0)  # 随机森林实例化
+selector = RFE(RFC_, n_features_to_select=11,step=10).fit(X,y)  # RFE实例化
+
+selected_features_indices = selector.support_  # 返回所有的特征是否被选中的布尔矩阵
+selected_feature_names = X.columns[selected_features_indices]  # 选中特征的列名
+
+print("Selected feature names: ", selected_feature_names)
+
+# 4.转换数据
+X_wrapper_features = selector.transform(X)
+
+# 5.交叉验证
+auc_scores = cross_val_score(RFC_,X_wrapper_features,y,cv=5,scoring='roc_auc').mean()
+
+print(auc_scores)
+
+#---------train描述符提取------------------train描述符提取-----------------------------------------
+'''
+# 提取指定列名的数据
+selected_columns = ['qed', 'HeavyAtomMolWt', 'ExactMolWt', 'BCUT2D_LOGPLOW', 'Chi0v', 'Kappa3',
+                    'LabuteASA', 'SMR_VSA10', 'SMR_VSA3', 'VSA_EState2', 'MolMR','Activity']
+train_data = df[selected_columns]
+
+#将提取的数据保存到文件中
+train_data.to_csv('F:/111/10PX_train_11_descriptor_RFE622.csv')
+
+#---------test描述符提取------------------test描述符提取------------------------------------------------------------------------------
+
+# 1.导入test
+df_test = pd.read_csv("F:/fluralaner_test_208descriptor.csv",index_col = 0)
+
+# 提取指定列名的数据
+selected_columns = ['qed', 'HeavyAtomMolWt', 'ExactMolWt', 'BCUT2D_LOGPLOW', 'Chi0v', 'Kappa3',
+                    'LabuteASA', 'SMR_VSA10', 'SMR_VSA3', 'VSA_EState2', 'MolMR','Activity']
+test_data = df_test[selected_columns]
+
+#将提取的数据保存到文件中
+test_data.to_csv('F:/111/10PX_test_11_descriptor_RFE622.csv')
+#'''
