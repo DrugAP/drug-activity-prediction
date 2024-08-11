8404da5713213a57826d621c5ca8a741365f8a96
will <59821233+DrugAP@users.noreply.github.com>
2024-08-12 00:27:52 +0800

    Update 3 PAP训练模型、评估模型、特征重要性、预测结果.py

--- PAP model 181/3 PAP训练模型、评估模型、特征重要性、预测结果.py
+++ PAP model 181/3 PAP训练模型、评估模型、特征重要性、预测结果.py
@@ -2,12 +2,12 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.metrics import confusion_matrix
-from sklearn.ensemble import RandomForestClassifier     # 随机森林(Random Forest) BaggingRegressor是随机森林专用
-from sklearn.metrics import recall_score,precision_score,accuracy_score,roc_auc_score,f1_score,matthews_corrcoef  # 导入了scikit-learn库中的召回率和ROC曲线面积两个评价指标。
+from sklearn.ensemble import RandomForestClassifier   
+from sklearn.metrics import recall_score,precision_score,accuracy_score,roc_auc_score,f1_score,matthews_corrcoef  
 from sklearn.model_selection import cross_val_score
 
 # 1.读取训练集和验证集 CSV 文件，获取每个化合物的分子描述符
-df = pd.read_csv("F:/8PX_train_14_descriptor_RFE622.csv",index_col = 0)   # index_col=0——第一列为索引值
+df = pd.read_csv("F:/8PX_train_14_descriptor_RFE622.csv",index_col = 0)   
 X = df.iloc[:, 0:-1].values
 y = df.iloc[:, -1].values
 print(X.shape)
@@ -18,29 +18,22 @@ from sklearn.metrics import RocCurveDisplay
 from sklearn.model_selection import StratifiedKFold
 from sklearn.metrics import auc
 from sklearn.feature_selection import RFE
-# 分类和ROC分析
-# 创建具有5个折叠的分层交叉验证对象
 cv = StratifiedKFold(n_splits=5)
 
-# 创建一个具有概率估计的随机森林分类器
 rf_clf = RandomForestClassifier(n_estimators=110,max_depth=8 ,min_samples_leaf=3,min_samples_split=2 ,random_state=0)
 #rf_clf.fit(X,y)
 
 #--------绘制ROC曲线-----------绘制ROC曲线----------------绘制ROC曲线-----------------------------------------------------
 
-tprs = []  # 用于存储每个折叠的真正率
-aucs = []  # 用于存储每个折叠的AUC值
-mean_fpr = np.linspace(0, 1, 100)  # 创建均匀分布的点以绘制ROC曲线
+tprs = []  
+aucs = []  
+mean_fpr = np.linspace(0, 1, 100)  
 
-# 创建绘图所需的图形和轴
 fig, ax = plt.subplots()
 
-# 遍历交叉验证的折叠
 for i, (train, test) in enumerate(cv.split(X, y)):
-    # 在训练数据上训练分类器
     rf_clf.fit(X[train], y[train])
 
-    # 从训练好的分类器和测试数据生成ROC曲线显示
     viz = RocCurveDisplay.from_estimator(
         rf_clf,
         X[test],
@@ -53,18 +46,16 @@ for i, (train, test) in enumerate(cv.split(X, y)):
 
     # 插值真正率以匹配均值均匀分布的点
     interp_tpr = np.interp(mean_fpr, viz.fpr, viz.tpr)
-    interp_tpr[0] = 0.0  # 确保第一个点为（0, 0）
-    tprs.append(interp_tpr)  # 存储插值后的真正率
-    aucs.append(viz.roc_auc)  # 存储计算得到的AUC值
+    interp_tpr[0] = 0.0  
+    tprs.append(interp_tpr) 
+    aucs.append(viz.roc_auc) 
 
-# 在ROC图上绘制“Chance”线
 ax.plot([0, 1], [0, 1], linestyle="--", lw=2, color="r", label="Chance", alpha=0.8)
 
-# 计算并绘制平均ROC曲线
 mean_tpr = np.mean(tprs, axis=0)
-mean_tpr[-1] = 1.0  # 确保最后一个点为（1, 1）
-mean_auc = auc(mean_fpr, mean_tpr)  # 计算平均AUC值
-std_auc = np.std(aucs)  # 计算AUC值的标准差
+mean_tpr[-1] = 1.0  
+mean_auc = auc(mean_fpr, mean_tpr) 
+std_auc = np.std(aucs) 
 ax.plot(
     mean_fpr,
     mean_tpr,
@@ -74,7 +65,6 @@ ax.plot(
     alpha=0.8,
 )
 
-# 计算并绘制平均值 +/- 1个标准差之间的区域
 std_tpr = np.std(tprs, axis=0)
 tprs_upper = np.minimum(mean_tpr + std_tpr, 1)
 tprs_lower = np.maximum(mean_tpr - std_tpr, 0)
@@ -87,14 +77,13 @@ ax.fill_between(
     label=r"$\pm$ 1 std. dev.",
 )
 
-# 设置图的限制和标签
 ax.set(
     xlim=[-0.05, 1.05],
     ylim=[-0.05, 1.05],
     title="Receiver operating characteristic",
 )
-ax.legend(loc="lower right")  # 在图上添加图例
-plt.savefig('F:/文章一 活性预测/Figure/fig 1 流程图/622 px ROC曲线.pdf', dpi=300)   # 保存图片到F盘，清晰度dpi
+ax.legend(loc="lower right") 
+plt.savefig('F:/px_ROC.pdf', dpi=300)  
 plt.show()
 
 #--------特征重要度-----------特征重要度---------------特征重要度-----------------------------------------------------
@@ -105,16 +94,14 @@ indices = np.argsort(importances)[::-1]
 num_features = len(importances)
 print(range(num_features))
 print(indices)
-# 将特征重要度以柱状图展示
 plt.figure(figsize=(15,10))
 plt.title("Feature importances")
 plt.bar(range(num_features), importances[indices], color="g", align="center")
 plt.xticks(range(num_features), [features[i] for i in indices], rotation='30')
 plt.xlim([-1, num_features])
-#plt.savefig('F:/文章一 活性预测/Figure/fig S1-15/px description importance622.pdf', dpi=300)   # 保存图片到F盘，清晰度dpi
+#plt.savefig('F:/px description importance.pdf', dpi=300)  
 plt.show()
 
-# 输出各个特征的重要度
 for i in indices:
     print("{0} - {1:.3f}".format(features[i], importances[i]))
 
@@ -131,20 +118,20 @@ y_proba_pred_test = rf_clf.predict_proba(X_test)
 print(y_proba_pred_test)
 test_cm = confusion_matrix(y_test, y_pred_test)
 print("test_test:{}".format(test_cm))
-acc_test = accuracy_score(y_test, y_pred_test)  # 准确率
+acc_test = accuracy_score(y_test, y_pred_test)
 print("acc_test:{}".format(acc_test))
-from sklearn import metrics   # 均方误差RMSE
+from sklearn import metrics  
 print("RMSE_csj:{}".format(np.sqrt(metrics.mean_squared_error(y_test, y_pred_test))))
-from sklearn.metrics import matthews_corrcoef  # 马修斯相关系数
+from sklearn.metrics import matthews_corrcoef  
 print("MCC:{}".format(matthews_corrcoef(y_test, y_pred_test)))
 
 #-------预测----------------------预测--------------------------预测--------------------------预测----------------
 
 #'''
 # 1.读取测试集 CSV 文件，获取每个化合物的分子描述符
-df = pd.read_csv("F:/8new px 14 descriptor622.csv",index_col = 0)   # index_col=0——第一列为索引值
+df = pd.read_csv("F:/8new px 14 descriptor622.csv",index_col = 0) 
 
-# 2.定义X 、Y（指纹数据集）将分子描述符转换为NumPy数组，并将其赋给变量X和y。其中，X包含所有指纹，y包含相应的活性标签（二进制分类）
+# 2.定义X 、Y
 X_cs = df.iloc[:, 0:-1].values
 y_cs = df.iloc[:, -1].values
 print(X.shape)
@@ -155,10 +142,10 @@ y_proba_pred_csj = rf_clf.predict_proba(X_cs)
 print(y_proba_pred_csj)
 cs_cm = confusion_matrix(y_cs, y_pred_csj)
 print("cs_cm:{}".format(cs_cm))
-acc_cs = accuracy_score(y_cs, y_pred_csj)  # 准确率
+acc_cs = accuracy_score(y_cs, y_pred_csj) 
 print("acc_cs:{}".format(acc_cs))
-from sklearn import metrics   # 均方误差RMSE
+from sklearn import metrics  
 print("RMSE_csj:{}".format(np.sqrt(metrics.mean_squared_error(y_cs, y_pred_csj))))
-from sklearn.metrics import matthews_corrcoef  # 马修斯相关系数
+from sklearn.metrics import matthews_corrcoef 
 print("MCC:{}".format(matthews_corrcoef(y_cs, y_pred_csj)))
 #'''
